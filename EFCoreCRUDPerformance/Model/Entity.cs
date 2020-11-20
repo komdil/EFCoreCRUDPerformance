0bd7edcdd78162e92b508d8a19beda4bcead8737
Dilshod Komilov <komilovd@silkroadprofessionals.com>
2020-11-20 14:33:38 +0500

    Add project files.

---
+++ EFCoreCRUDPerformance/Model/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCRUDPerformance.Model
+{
+    public class Entity
+    {
+        public Guid Guid { get; set; }
+    }
+}
---
+++ EFCoreCRUDPerformance/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCRUDPerformance.Model
+{
+    class User
+    {
+    }
+}
